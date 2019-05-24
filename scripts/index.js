38d61c2a04390c90ca663b1dc7cde679c969baa4
JasonKirn <jkirn@csumb.com>
2019-05-24 14:06:19 -0700

    Added Express code

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-console.log("henlo world");
+console.log("test console log");
+//Ties everything together
+const express = require('express')
+const app = express()
+const port = 3000
+var path = require("path");
+//Routing for Home Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'../index.html')))
+console.log(__dirname);
+//Init Server and Run Code
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
