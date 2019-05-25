a434457e0f3e7336c5d46cb3ccc60f64b6848208
JasonKirn <jkirn@csumb.com>
2019-05-24 19:40:16 -0700

    Fixed style

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
 //This code runs the express server
 console.log("test console log");
+
+
 //Ties everything together
 const express = require('express')
 const app = express()
 const port = 3000
 var path = require("path");
+
+
 //Routing for Home Page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'../index.html')))
+
 
 //Init Server and Run Code
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
