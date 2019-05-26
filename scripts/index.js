6986ab3abc64e64035548147b3f92a47bad71396
Christopher Munden <cmunden@alumni.unca.edu>
2019-05-26 12:05:24 -0400

    Added keys to gitignore

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,6 @@ var pathVar = require("path");
 
 //Routing for Home Page
 app.get('/', (req, res) => res.sendFile(pathVar.join(__dirname,'../index.html')))
-
-
 app.get('/himjim', (req, res) => {
     res.send('This is the rootinest tootinest webpage himjim')
 })
