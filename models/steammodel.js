58ab8d5a1dfc5a4f1caa82ff180aeea63613e706
Christopher Munden <cmunden@alumni.unca.edu>
2019-05-26 21:30:05 -0400

    Added testcode for Steam API

--- models/steammodel.js
+++ models/steammodel.js
@@ -1,6 +1,11 @@
 //Test code for Steam API below
 const apikey = require("../keys/steamapikeys.json");
+const testUser = "76561197979972334"
+function getGames() {
+   var games = $.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + apikey.key + "&steamid=76561197960434622" + testUser + "&format=json");
+   console.log(games);
+}
 class {
-    
+
 }
   
