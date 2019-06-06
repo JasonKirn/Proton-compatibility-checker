a4a3e4f601cae71baad5b5dd463d91a7f280394c
Jason Kirn <jkirn@csumb.edu>
2019-06-06 00:04:33 -0700

    Created temp ajax key call, updating later for security

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,13 +1,18 @@
 //const apikey = require("../keys/steamapikey.json");
 //const testUser = "76561197979972334";
-apikey = require("../keys/steamapikey.json");
+//apikey = require("../keys/steamapikey.json");
 testUser = "76561197979972334";
-console.log(apikey);
+//console.log(apikey);
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+/*$.ajax({url: "../keys/steamapikey.json", success: function(result){
+    console.log(result);
+}});*/
+
 //Goals:
 
 //1. Call the steammodel
 //2. Send the results of the steammodel to the view for the user
-var steamModel = require("../models/steammodel");
+//var steamModel = require("../models/steammodel");
 //var modelCallResults = steamModel.getGames(testUser, apikey);
 
 //var test = steamModel.test();
--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,7 @@ var pathVar = require("path");
 app.get('/', (req, res) => res.sendFile(pathVar.join(__dirname,'../index.html')))
 app.get("/scripts/main.js", (req, res) => res.sendFile(pathVar.join(__dirname,'../scripts/main.js')))
 app.get("/scripts/controller.js", (req, res) => res.sendFile(pathVar.join(__dirname,'../scripts/controller.js')))
+app.get("/keys/steamapikey.json", (req, res) => res.sendFile(pathVar.join(__dirname,'../keys/steamapikey.json')))
 app.get('/himjim', (req, res) => {
     res.send('This is the rootinest tootinest webpage himjim')
 })
