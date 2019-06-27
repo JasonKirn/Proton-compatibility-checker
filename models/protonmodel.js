7ead8e28d15ea329e37593b2969435beb737d9df
Christopher Munden <cmunden@alumni.unca.edu>
2019-06-27 15:22:33 -0400

    Added average rating system.

--- models/protonmodel.js
+++ models/protonmodel.js
@@ -15,7 +15,41 @@ exports.getRating = function (appid) {
         res.on('end', () => {
             //Parses data (which is a json object) into parasedata variable
             parseddata = JSON.parse(data);
-            console.log(parseddata.rating)
+            console.log(averageRating(parseddata))
         })
     })
+}
+
+averageRating = function (reportList) {
+    var maxNumberOfReports = 5;
+    var addedReportRatings = 0;
+    if (reportList.length < maxNumberOfReports) {
+        maxNumberOfReports = reportList.length;
+    }
+    for(i = 0; i < maxNumberOfReports; i++ ) {
+        addedReportRatings += convertRating(reportList[i].rating)
+    }
+    return (Math.round(addedReportRatings/maxNumberOfReports))
+}
+
+convertRating = function (rating) {
+    var convertedRating
+    switch (rating) {
+        case "Platinum":
+            convertedRating = 5;
+            break;
+        case "Gold":
+            convertedRating = 4;
+            break;
+        case "Silver":
+            convertedRating = 3;
+            break;
+        case "Bronze":
+            convertedRating = 2;
+            break;
+        case "Borked":
+            convertedRating = 1;
+            break;
+    }
+    return convertedRating;
 }
