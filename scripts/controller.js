eb9fd06db97d564cb779c3c150cbf2af2d2ecee7
Christopher Munden <cmunden@alumni.unca.edu>
2019-06-27 00:51:52 -0400

    Preparing for Merge.

--- scripts/controller.js
+++ scripts/controller.js
@@ -6,7 +6,6 @@ var apikey = require("../keys/steamapikey.json")
 //1. Call the steammodel
 //2. Send the results of the steammodel to the view for the user
 var steamModel = require("../models/steammodel.js");
-var modelCallResults = steamModel.getGames(testUser, apikey.key); //remove later
 
 function callSteamModelAndDisplayResults() {
 
