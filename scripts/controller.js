6e1fe8592399868104c2b11dd0984ad1172609bc
Christopher Munden <cmunden@alumni.unca.edu>
2019-07-26 00:05:52 -0400

    Resolving conflicts in controller.js

--- scripts/controller.js
+++ scripts/controller.js
@@ -2,15 +2,6 @@ var testUser = "76561197979972334";
 var apikey = require("../keys/steamapikey.json");
 
 
-<<<<<<< HEAD
-//1. Call the steammodel
-//2. Send the results of the steammodel to the view for the user
-var steamModel = require("../models/steammodel.js");
-var protonModel = require("../models/protonmodel.js");
-
-steamModel.getGames(testUser, apikey.key)
-   .then(finalData => {protonModel.processGameList(finalData)})
-=======
 var SteamModel = require("../models/steammodel.js");
 var GameListModel = require("../models/gamelistmodel.js");
 
@@ -19,7 +10,6 @@ var gameListModel = new GameListModel();
 
 steamModel.retriveGames(testUser)
    .then(unprocessedSteamData => { proccessSteamData(unprocessedSteamData) })
->>>>>>> master
    .catch(console.log(Error))
 
 
