bee888ba46c570659704c204c11242976051ad42
Christopher Munden <cmunden@alumni.unca.edu>
2019-08-06 19:19:57 -0400

    Sucessfully added post function to retrive steamgames from input box.

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,26 @@
  */
 startServer = function () {
     //Variable declartions needed to start express
-    const expresss = require('express')
-    const app = expresss()
+    const express = require('express')
+    const app = express()
     const port = 3000
     const pathVar = require("path");
+    const bodyParser = require("body-parser");
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+
 
 
     //Route declerations for webpage traversal
     app.get('/', (req, res) => res.sendFile(pathVar.join(__dirname, '../index.html')))
     app.get('/scripts/main.js', (req, res) => res.sendFile(pathVar.join(__dirname, '../scripts/main.js')))
-    app.post("/getSteamGames", (req, res) => console.log("command recieved"))
-
+    app.post("/getSteamGames", (req, res) => getSteamGames(req, res))
+    
+    getSteamGames = async function(req, res) {
+       console.log(await controller.retriveGames(req.body.userid))
+    }
     //Code to start server
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
     //Start and run Controller
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $("#useridsubmit").on("click", function() {
-        $.post("getSteamGames")
+        $.post("getSteamGames", {
+            userid: $("#userIdBox").val()
+        })
     })
 })
